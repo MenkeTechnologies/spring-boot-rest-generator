a597b6ea362f6cbde9f411a60983055772aea904
MenkeTechnologies <linux.dev25@gmail.com>
2019-05-14 10:09:50 -0400

    include table name

--- src/main/java/com/jakobmenke/bootrestgenerator/App.java
+++ src/main/java/com/jakobmenke/bootrestgenerator/App.java
@@ -13,20 +13,20 @@ public class App {
     private final static String PACKAGE = "com/jakobmenke/boot";
     private static final String FILE_NAME = "dump.sql";
 
-    private void writeEntities(ArrayList<Table> tables) {
+    private void writeTemplates(ArrayList<Table> tables) {
         Templates templates = new Templates();
         for (Table entityName : tables) {
             String entityTemplate = templates.getEntityTemplate(entityName, PACKAGE);
-            createFile("entity", entityName.getName() + ".java", entityTemplate);
+            createFile("entity", entityName.getEntityName() + ".java", entityTemplate);
 
-            String serviceTemplate = templates.getServiceTemplate(PACKAGE, entityName.getName());
-            createFile("rest", entityName.getName() + "Resource.java", serviceTemplate);
+            String serviceTemplate = templates.getServiceTemplate(PACKAGE, entityName.getEntityName());
+            createFile("rest", entityName.getEntityName() + "Resource.java", serviceTemplate);
 
-            String daoTemplate = templates.getDaoTemplate(PACKAGE, entityName.getName());
-            createFile("dao", entityName.getName() + "Dao.java", daoTemplate);
+            String daoTemplate = templates.getDaoTemplate(PACKAGE, entityName.getEntityName());
+            createFile("dao", entityName.getEntityName() + "Dao.java", daoTemplate);
 
-            String repositoryTemplate = templates.getRepositoryTemplate(PACKAGE, entityName.getName());
-            createFile("repositories", entityName.getName() + "Repository.java", repositoryTemplate);
+            String repositoryTemplate = templates.getRepositoryTemplate(PACKAGE, entityName.getEntityName());
+            createFile("repositories", entityName.getEntityName() + "Repository.java", repositoryTemplate);
         }
     }
 
@@ -55,17 +55,16 @@ public class App {
 
         ArrayList<String> words = new ArrayList<>();
 
-        try (Scanner scanner = new Scanner(Objects.requireNonNull(App.class.getClassLoader().getResourceAsStream(FILE_NAME)))) {
-            while (scanner.hasNext()) {
+        getWords(words);
 
-                String word = scanner.next();
+        parseWords(tables, words);
 
-                words.add(word);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        App app = new App();
 
+        app.writeTemplates(tables);
+    }
+
+    private static void parseWords(ArrayList<Table> tables, ArrayList<String> words) {
         for (int i = 0; i < words.size(); i++) {
             String word = words.get(i);
             if (word.equalsIgnoreCase("create")) {
@@ -73,9 +72,10 @@ public class App {
                 if (nextWord.equalsIgnoreCase("table")) {
                     String tableWord = words.get(i + 2);
                     List<String> comps = Arrays.stream(tableWord.replace("`", "").split("_")).collect(Collectors.toList());
-                    String tablename = comps.stream().map(w -> w.substring(0, 1).toUpperCase() + w.substring(1).toLowerCase()).collect(Collectors.joining(""));
+                    String entityName = comps.stream().map(w -> w.substring(0, 1).toUpperCase() + w.substring(1).toLowerCase()).collect(Collectors.joining(""));
                     Table table = new Table();
-                    table.setName(tablename);
+                    table.setEntityName(entityName);
+                    table.setTableName(tableWord.replaceAll("`", ""));
                     tables.add(table);
                 }
             }
@@ -93,10 +93,19 @@ public class App {
                 tables.get(tables.size() - 1).getColumns().add(new Column(columnName, camelName, datatype, javaType));
             }
         }
+    }
 
-        App app = new App();
+    private static void getWords(ArrayList<String> words) {
+        try (Scanner scanner = new Scanner(Objects.requireNonNull(App.class.getClassLoader().getResourceAsStream(FILE_NAME)))) {
+            while (scanner.hasNext()) {
 
-        app.writeEntities(tables);
+                String word = scanner.next();
+
+                words.add(word);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private static String getJavaType(String datatype) {
@@ -117,11 +126,16 @@ public class App {
         if (pattern.matcher(datatype).matches()) {
             return "LocalDate";
         }
+        pattern = Pattern.compile("\\bbit[()\\d]*");
+        if (pattern.matcher(datatype).matches()) {
+            return "String";
+        }
         pattern = Pattern.compile("\\btimestamp[()\\d]*");
         if (pattern.matcher(datatype).matches()) {
             return "LocalDateTime";
         }
 
-        return "error";
+        //default to string
+        return "String";
     }
 }
