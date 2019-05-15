97e333451932f40270cc862b8db031a347bd250b
I'jaaz Tello <mitello@wccnet.edu>
2019-05-15 13:53:56 -0400

    Added additional camelcase tests

--- src/test/java/com/jakobmenke/bootrestgenerator/AppTest.java
+++ src/test/java/com/jakobmenke/bootrestgenerator/AppTest.java
@@ -22,6 +22,10 @@ class AppTest {
         Column expected2 = new Column("@ManyToOne", "TESTING_ID", "testing", null, "Testing");
         String foreignKey1 = "FOREIGN KEY (`TESTING_ID`)";
         assertEquals(expected2, App.getId(foreignKey1));
+
+        Column expected3 = new Column("@ManyToOne", "TESTING_NO_BAD_ID", "testingNoBad", null, "TestingNoBad");
+        String foreignKey2 = "FOREIGN KEY (`TESTING_NO_BAD_ID`)";
+        assertEquals(expected3, App.getId(foreignKey2));
     }
     @Test
     void testToCaps() {
@@ -31,5 +35,11 @@ class AppTest {
         assertEquals("This will be capitalized", App.firstLetterToCaps(testCase0));
         assertEquals("", App.firstLetterToCaps(testCase1));
         assertEquals("1019 test", App.firstLetterToCaps(testCase2));
+    }
+
+    @Test
+    void testCamelName() {
+        assertEquals("helloWorld", App.camelName("HELLO_WorLD"));
+        assertEquals("helloW", App.camelName("HELLO_W"));
     }
 }
